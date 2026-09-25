cc3645d576d40e011fb6b94e33d7aa3ff3f4e259
Joshua Peek <josh@users.noreply.github.com>
2026-09-24 21:31:06 -0700

    ext-postgres-operator-chart: commit generated CRD module

--- pkgs/movetokube/ext-postgres-operator-chart.nix
+++ pkgs/movetokube/ext-postgres-operator-chart.nix
@@ -5,6 +5,8 @@ kubepkgs.fetchhelm {
   version = "3.0.0";
   hash = "sha256-sf4MgShIK+I5fNoCO1aMqFw+T9SW5hY7qS3awQo8XU0=";
 
+  crds.file = ../../crds/ext-postgres-operator.nix;
+
   meta = {
     description = "Helm chart for the external PostgreSQL operator";
     homepage = "https://github.com/movetokube/postgres-operator";
